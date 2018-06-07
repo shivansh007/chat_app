3aedeabad52dc9da345a815151bae7626458b36e
shivansh007 <shivanshnlw@gmail.com>
2018-06-08 00:17:42 +0530

    Add validations and password secure in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	has_secure_password
+	validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
+	validates :password, presence: true, allow_nil: true, length: { in: 6..20 }
+	validates :email, presence: true, uniqueness: true, , format: { with: /\A\S+@.+\.\S+\z/, message: "Invalid email" }
 end
