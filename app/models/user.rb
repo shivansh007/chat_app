d18c6db15f809d30d62e6d1cb5131c0a3e42714c
shivansh007 <shivanshnlw@gmail.com>
2018-06-08 00:40:19 +0530

    Add controller, route, and fix extra comma in model for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
 	validates :password, presence: true, allow_nil: true, length: { in: 6..20 }
-	validates :email, presence: true, uniqueness: true, , format: { with: /\A\S+@.+\.\S+\z/, message: "Invalid email" }
+	validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: "Invalid email" }
 end
