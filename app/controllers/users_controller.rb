6b3b28fa9df7a8557dea5746234f465a2537a99b
shivansh007 <shivanshnlw@gmail.com>
2018-06-08 00:52:50 +0530

    Add controller for user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
+
+  def index
+    users = User.all
+    render json: { users: users }, status: :ok
+  end
+
+  def show
+    render json: { user: @user }, status: :ok
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { user: @user }, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: { user: @user }, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    render json: nil, status: :ok
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :is_verified)
+  end
+end
