1f7b893f9bfb564fe59ac46ee7d34da11897ff67
shivansh007 <shivanshnlw@gmail.com>
2018-06-08 16:18:46 +0530

    Add auth token to user model, and sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-	has_secure_password
-	validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: "Invalid email" }
-	validates :password, presence: true, allow_nil: true, length: { in: 6..20 }
+  has_secure_password
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/, message: "Invalid email" }
+  validates :password, presence: true, allow_nil: true, length: { in: 6..20 }
+
+  def generate_auth_token
+    token = SecureRandom.hex
+    self.update_columns(auth_token: token)
+    token
+  end
+
+  def invalidate_auth_token
+    self.update_columns(auth_token: nil)
+  end
 end
